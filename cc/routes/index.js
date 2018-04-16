79d3c3517c60acf9ab6ac7a2887e3bdb3692e6b4
Troy Crawford <tacrawford91@gmail.com>
2018-04-15 20:30:10 -0500

    added cheerio and request. Also added index to scrape routes

--- cc/routes/index.js
+++ cc/routes/index.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const scrapeRoutes = require("./scrape")
 
 
 router.use("/api", apiRoutes);
-router.use("/scrape/news", require("./scrape/scrape-news"));
-router.use("/scrape/jobs", require("./scrape/scrape-jobs"));
-router.use("/scrape/events", require("./scrape/scrape-events"));
+router.use("/scrape", scrapeRoutes)
 
 module.exports = router;
