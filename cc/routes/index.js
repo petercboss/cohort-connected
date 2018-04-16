cbe9136bf3795c966ffc07283e07f727ecbf25d2
Troy Crawford <tacrawford91@gmail.com>
2018-04-15 21:28:21 -0500

    api routes working

--- cc/routes/index.js
+++ cc/routes/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-// const apiRoutes = require("./api");
+const apiRoutes = require("./api");
 const scrapeRoutes = require("./scrape");
 
 
-// router.use("/api", apiRoutes);
+router.use("/api", apiRoutes);
 router.use("/scrape", scrapeRoutes);
 
 module.exports = router;
