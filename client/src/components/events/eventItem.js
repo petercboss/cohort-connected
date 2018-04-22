084aa1979c05f394e8c0a193ea2a5b7097e5e65e
Rachel Brown <rbrown511@gmail.com>
2018-04-22 00:18:10 -0500

    changing navpills to buttons from tabs

--- client/src/components/events/eventItem.js
+++ client/src/components/events/eventItem.js
@@ -8,9 +8,9 @@ import 'moment-timezone';
 import './eventsList.css'
 
 export const EventItem = props => (
-    <li className={props.bk % 2 === 0 ? 'list-group-item bk-dark' : 'list-group-item bk-light'} id={props.id}>
-        <a href={props.link} className='event-link'><h3 className='event-title'>{props.title}</h3></a>
-        <h4><i className='fa fa-calendar' aria-hidden='true'></i> <Moment format='dddd, MMMM Do YYYY, h:mm a'>{props.date}</Moment></h4>
+    <li className={props.bk % 2 === 0 ? 'list-group-item bk-light' : 'list-group-item bk-dark'} id={props.id}>
+        <a href={props.link} className='event-link'><h4 className='event-title'>{props.title}</h4></a>
+        <h5><i className='fa fa-calendar' aria-hidden='true'></i> <Moment format='dddd, MMMM Do YYYY, h:mm a'>{props.date}</Moment></h5>
         <p className='hosted-by'>Hosted By {props.organizer}</p>
         <p>{props.categories.map((cat, i) => {
             return <button key={i} data-cat={cat} className='category-btn'><i className='fa fa-tag' aria-hidden='true'></i> {cat}</button>
