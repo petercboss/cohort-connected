4bdd8f5a98c4f8ef95ea9543ae4796c149bc2606
Scott Saltzman <salty923@gmail.com>
2018-04-27 19:16:43 -0500

    add modal in jobs

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// import Demo from './containers/demo'
+import Demo from './containers/demo'
 
 // import { card as cardStyle, hr as hrStyle } from './components/userCard/styles';
 
 ReactDOM.render(
     <div>
-        <App />
+        <Demo />
     </div>,
     document.getElementById('root')
 ) 
