e8e4c35a9504039cc7a6f93126d5534179ee24c2
Scott Saltzman <salty923@gmail.com>
2018-05-02 21:40:14 -0500

    added thumbs up and down with comments to jobs

--- models/job.js
+++ models/job.js
@@ -5,7 +5,13 @@
     const jobSchema = new Schema({
         summary: { type: String, required: true },
         title: { type: String, required: true },
-        link: { type: String, required: true }
+        link: { type: String, required: true },
+        thumbsUp: { type: Number, default: 0 },
+        thumbsDown: { type: Number, default: 0 },
+        comment: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }]
     });
 
     const Job = mongoose.model("Job", jobSchema);
