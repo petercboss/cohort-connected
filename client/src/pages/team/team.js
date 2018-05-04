b5f5cbac52fce96880f0e869c415986e04b25af0
Rachel Brown <rbrown511@gmail.com>
2018-05-04 00:25:07 -0500

    last tweaks to team page

--- client/src/pages/team/team.js
+++ client/src/pages/team/team.js
@@ -11,7 +11,7 @@ class Team extends Component {
                 title: 'Codesmith',
                 image: 'https://avatars1.githubusercontent.com/u/30506899?s=400&v=4',
                 bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et velit non neque aliquam dignissim sit amet ac mauris. Mauris ultrices, erat at vestibulum semper, est sem eleifend nulla, eu feugiat enim lacus sed dolor. Proin dolor sapien, egestas vitae mauris quis, pretium convallis orci. Pellentesque ac risus a metus vehicula tristique. Integer nec massa.',
-                portfolio: 'https://peterboss.com/',
+                portfolio: 'peterboss.com',
                 github: 'https://github.com/petercboss',
                 linked: 'https://www.linkedin.com/in/peter-boss/'
             },
@@ -19,8 +19,8 @@ class Team extends Component {
                 name: 'Rachel B. Brown',
                 title: 'Codesmith',
                 image: 'https://avatars0.githubusercontent.com/u/33067290?s=400&v=4',
-                bio: 'Rachel’s career began in law, with a fun little offshoot into the magical world of cooking for television. Now she’s a badass developer, excited to take on the ever-changing landscape of technology. The diversity of her background and experiences is reflected in her fresh and unique approach to web development. When she’s not in front of a computer, her hobbies include travel, golf and boxing.',
-                portfolio: 'https://rachelb.tech/',
+                bio: 'Rachel’s career began in law, with a fun little offshoot into the magical world of cooking for television. Now she’s a badass coder, with a passion for problem solving and a strong eye for design. When she’s not in front of a computer, her hobbies include travel, golf and boxing.',
+                portfolio: 'rachelb.tech',
                 github: 'https://github.com/rbrown511',
                 linked: 'https://www.linkedin.com/in/rachel-basia-brown-83b5b248/'
             },
@@ -29,7 +29,7 @@ class Team extends Component {
                 title: 'Codesmith',
                 image: 'https://avatars0.githubusercontent.com/u/30868593?s=400&v=4',
                 bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et velit non neque aliquam dignissim sit amet ac mauris. Mauris ultrices, erat at vestibulum semper, est sem eleifend nulla, eu feugiat enim lacus sed dolor. Proin dolor sapien, egestas vitae mauris quis, pretium convallis orci. Pellentesque ac risus a metus vehicula tristique. Integer nec massa.',
-                portfolio: 'https://troycrawford.tech/',
+                portfolio: 'troycrawford.tech',
                 github: 'https://github.com/tacrawford91',
                 linked: 'https://www.linkedin.com/in/tacrawford91/'
             },
@@ -38,7 +38,7 @@ class Team extends Component {
                 title: 'Codesmith',
                 image: 'https://media.licdn.com/dms/image/C4E03AQEIvbiWogQDAg/profile-displayphoto-shrink_800_800/0?e=1530122400&v=beta&t=amzL1Ioq9RTfARFOXY0IpOW6OHYFOmzmUlxq5-cEDfE',
                 bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et velit non neque aliquam dignissim sit amet ac mauris. Mauris ultrices, erat at vestibulum semper, est sem eleifend nulla, eu feugiat enim lacus sed dolor. Proin dolor sapien, egestas vitae mauris quis, pretium convallis orci. Pellentesque ac risus a metus vehicula tristique. Integer nec massa.',
-                portfolio: 'https://shawnfeiz.github.io/Portfolio/',
+                portfolio: 'shawnfeiz.github.io/Portfolio',
                 github: 'https://github.com/ShawnFeiz',
                 linked: 'https://www.linkedin.com/in/shawnfeiz/'
             },
@@ -47,7 +47,7 @@ class Team extends Component {
                 title: 'Codesmith',
                 image: 'https://avatars0.githubusercontent.com/u/13115712?s=460&v=4',
                 bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et velit non neque aliquam dignissim sit amet ac mauris. Mauris ultrices, erat at vestibulum semper, est sem eleifend nulla, eu feugiat enim lacus sed dolor. Proin dolor sapien, egestas vitae mauris quis, pretium convallis orci. Pellentesque ac risus a metus vehicula tristique. Integer nec massa.',
-                portfolio: 'https://salty923.github.io/Bootstrap-Portfolio/',
+                portfolio: 'salty923.github.io/Bootstrap-Portfolio/',
                 github: 'https://github.com/Salty923',
                 linked: 'https://www.linkedin.com/in/scott-saltzman-33949b56/'
             }
@@ -72,7 +72,11 @@ class Team extends Component {
         <Container>
             <Row>
                 <Col size='md-12' className='teamPage animated fadeInUp'>
-                    <h1 className='teamWork'>BUILT ON<br/>TEAMWORK.</h1>
+                    <h1 className='teamWork'>
+                        <span className='teamBlurb'><b>YOU</b> bring CohortConnected to life.<br/>
+                        These fun-seekers just wrote the source code.</span>
+                        BUILT ON<br/><b>TEAMWORK</b>.
+                    </h1>
                     { 
                         this.state.team.map((member, i) => {
                             return (
@@ -83,7 +87,12 @@ class Team extends Component {
                                     <h4 className='memberTitle'>{member.title}</h4>
                                     <p className='memberBio'>{member.bio}</p>
                                     <hr className='link-divider' />
-                                    <h4 className='memberSites'><i class="fab fa-github"></i>&nbsp;<i class="fab fa-linkedin-in"></i>&nbsp;<a href={member.portfolio} target='_blank' rel='noopener noreferrer'>Portfolio</a></h4>
+
+                                    <h4 className='memberSites'>
+                                        <a href={member.linked} target='_blank' rel='noopener noreferrer'><i className='site-icons fab fa-linkedin-in'></i></a>
+                                        <a href={member.github} target='_blank' rel='noopener noreferrer'><i className='site-icons fab fa-github'></i></a>
+                                        <a className='memberPortfolio' href={'https://' + member.portfolio} target='_blank' rel='noopener noreferrer'>{member.portfolio.split('/')[0]}</a>
+                                    </h4>
                                 </div>
                                 <div className='clearfix' />
                             </div> )
