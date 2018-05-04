8e14d9b90e98e627df4409d6b5a427b1a78de04b
Troy Crawford <tacrawford91@gmail.com>
2018-05-03 21:33:11 -0500

    sort unread messages, added date, and highlight

--- client/src/components/ChatMessage/ChatMessage.js
+++ client/src/components/ChatMessage/ChatMessage.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 // external stylesheet and bootstrap style components
 import './ChatMessageArea.css';
 import {Row} from '../../components/Grid';
+import Moment from 'react-moment';
 
   
 class ChatMessage extends Component {
 
     componentDidMount() {
         this.scrollToBottom(this.props.index);
+        console.log(new Date(+this.props.date))
     }  
     scrollToBottom = (ref) => {
         this.refs[ref].scrollIntoView({ behavior: "smooth",block: "end", inline: "nearest" });
@@ -24,18 +26,18 @@ class ChatMessage extends Component {
                     <div className='meChatSent'>
                         <h3 className='meChatName'>{this.props.name}</h3>         
                         <div className='myChatBubble'>
-                            <p>{this.props.chatMessage}</p>
+                            <p>{this.props.chatMessage} <span className='messageDate'><Moment fromNow>{new Date(+this.props.date)}</Moment></span></p>
                         </div>
                     </div>
                 </Row>
-            </li>)
+            </li>) 
             :
             (<li ref={this.props.index}>
                 <Row className='chatRow'>
                     <div className='senderUser'>
                         <h3 className='senderChatName'>{this.props.name}</h3>         
                         <div className='chatBubble'>
-                            <p>{this.props.chatMessage}</p>
+                            <p>{this.props.chatMessage} <span className='messageDate'><Moment fromNow>{new Date(+this.props.date)}</Moment></span></p>
                         </div>
                     </div>
                 </Row>
--- client/src/components/ChatMessage/ChatMessageArea.js
+++ client/src/components/ChatMessage/ChatMessageArea.js
@@ -8,7 +8,7 @@ export const ChatMessageArea = (props) => {
     return (
     <div className='ChatMessageArea'>
         <ul className='chatUL'>
-        {(props.messages[0] !== 0) ? (props.children) : (<h1></h1>)}
+        {(props.messages[0] !== 0) ? (props.children) : (<div className='selectUserDiv'><p>Please Select A User From The Left To Write A Message</p></div>)}
         </ul>
     </div>
     )
