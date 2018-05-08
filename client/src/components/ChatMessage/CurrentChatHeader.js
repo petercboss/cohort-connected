7accf0f83a65fa7603371add509767fa05df124f
Troy Crawford <tacrawford91@gmail.com>
2018-05-07 20:20:28 -0500

    message style cleanup. Styled all scroll bars as well

--- client/src/components/ChatMessage/CurrentChatHeader.js
+++ client/src/components/ChatMessage/CurrentChatHeader.js
@@ -7,7 +7,7 @@ export const CurrentChatHeader = props => (
         <div className='CurrentChatHeader'>
         {(props.currentUser.firstName) ? (
         <div>
-        <img src={props.currentUser.profilePicURL} alt="title" className='chatImage' />
+        <img src={props.currentUser.profilePicURL} alt="title" className='currentChatImage' />
         <h1 className='currentName'>{props.currentUser.firstName} {props.currentUser.lastName}</h1>
         </div>) : (<div className='selectUserDiv'><p>Select a User from the Left to Start Messaging</p></div>)
         }
